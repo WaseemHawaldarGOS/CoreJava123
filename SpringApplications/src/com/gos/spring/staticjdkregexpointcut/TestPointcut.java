c2c29f63123d8e532f0fe46661efd85c179942ba
WaseemHawaldarGOS <waseemhawaldar@gmail.com>
2019-10-07 11:20:46 +0530

    Adding Spring dependency injection example.

--- SpringApplications/src/com/gos/spring/staticjdkregexpointcut/TestPointcut.java
+++ SpringApplications/src/com/gos/spring/staticjdkregexpointcut/TestPointcut.java
@@ -20,7 +20,7 @@ public class TestPointcut {
 		//below line works in eclipse but not in intellij
 		//BeanFactory beans = new XmlBeanFactory(new FileSystemResource("file:src/com/gos/spring/spring1dot2aop/beforeadvice/aopadvices-beans.xml"));
 
-		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("conf/pointcuts-beans.xml");
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("conf/jdkregexpointcuts-beans.xml");
 		Target myser = (Target) applicationContext.getBean("pointcutService");
 		
 		System.out.println("From main, calling Mylogic(0)");
