741e3ac6bbf0a96ebf10cd3e43291f8fe3147fbb
WaseemHawaldarGOS <waseemhawaldar@gmail.com>
2021-01-26 12:58:58 +0530

    updated code

--- CoreJava/src/com/gos/collections/LinkedHashSetExample.java
+++ CoreJava/src/com/gos/collections/LinkedHashSetExample.java
@@ -13,7 +13,7 @@ public class LinkedHashSetExample {
     public static void main(String args[]) {
         boolean status = false;
         // create a hash set
-        LinkedHashSet<String> hs = new LinkedHashSet<String>();
+        LinkedHashSet hs = new LinkedHashSet<>();
 // add elements to the hash set
 
         hs.add("B");
@@ -41,7 +41,11 @@ public class LinkedHashSetExample {
         System.out.println(hs.toString());
 
         System.out.println("Trying to add heterogenous data");
-        //hs.add(100);//this line of code fails
+        status = hs.add(100);//this line of code fails
+        if(!status)
+            System.out.println("Additional of heterogenous data succeeded");
+        else
+            System.out.println("Additional of heterogenous data failed");
 
 
     }
