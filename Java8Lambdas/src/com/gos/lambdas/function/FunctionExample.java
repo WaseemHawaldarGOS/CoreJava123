28291332426ad8580c5c5bad330b3c250ef5fec1
WaseemHawaldarGOS <waseemhawaldar@gmail.com>
2021-07-09 11:36:21 +0530

    updated

--- Java8Lambdas/src/com/gos/lambdas/function/FunctionExample.java
+++ Java8Lambdas/src/com/gos/lambdas/function/FunctionExample.java
@@ -14,6 +14,12 @@ https://www.javabrahman.com/java-8
 https://www.javabrahman.com/java-8/java-8-java-util-function-function-tutorial-with-examples/
  */
 
+/**
+ * what compose do?
+ *
+ *  In simple terms the order of functions is interchanged as compared to andThen, eg. function1.compose(function2)
+ *  first function2 will be called an then function1.
+ */
 
 public class FunctionExample {
 	public static void main(String args[]) {
@@ -22,15 +28,13 @@ public class FunctionExample {
 		/**
 		 * Invocation of primary apply method
 		 */
-		Function<Employee, String> funcEmpToString = (Employee e) -> {
-			return e.getName();
-		};
+		/*Function<Employee, String> funcEmpToString = (Employee e) ->  e.getName();
 		
 		List<Employee> employeeList = Arrays.asList(new Employee("Tom Jones", 45), new Employee("Harry Major", 25),
 				new Employee("Ethan Hardy", 65), new Employee("Nancy Smith", 15),
 				new Employee("Deborah Sprightly", 29));
 		List<String> empNameList = convertEmpListToNamesList(employeeList, funcEmpToString);
-		empNameList.forEach(System.out::println);
+		empNameList.forEach(System.out::println);*/
 		
 		
 		
@@ -55,7 +59,7 @@ public class FunctionExample {
 		/**
 		 * Invocation of compose method
 		 */
-		/*Function<Employee, String> funcEmpToString = (Employee e) -> {
+		Function<Employee, String> funcEmpToString = (Employee e) -> {
 			return e.getName();
 		};
 		
@@ -69,7 +73,7 @@ public class FunctionExample {
 				new Employee("Ethan Hardy", 65), new Employee("Nancy Smith", 15),
 				new Employee("Deborah Sprightly", 29));
 		List<String> empNameList = convertEmpListToNamesList(employeeList, funcEmpToString.compose(funcEmpToString2));
-		empNameList.forEach(System.out::println);*/
+		empNameList.forEach(System.out::println);
 		
 		/**
 		 * Invocation of identity method. Not much clarity on this.
