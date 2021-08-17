58befef003871cf69e08864941dcb6a2f2c010e9
Waseem Hawaldar <waseemhawaldar@gmail.com>
2021-08-17 11:59:39 +0530

    Added Java 8 new features.

--- Java8Lambdas/src/com/gos/java8optional/MapOrFlatMap.java
+++ Java8Lambdas/src/com/gos/java8optional/MapOrFlatMap.java
@@ -1,6 +1,10 @@
 package com.gos.java8optional;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /*
 https://beginnersbook.com/2017/10/java-8-optional-class/
@@ -22,8 +26,28 @@ public class MapOrFlatMap {
                 +anotherOptional.map(str -> str.map(String::toUpperCase)));
         //Optional<Optional<String>>    -> flatMap -> Optional<String>
         System.out.println("Optional.flatMap: "
-                +anotherOptional.flatMap(str -> str.map(String::toUpperCase)));
+                +anotherOptional.flatMap(str -> str.map(String::toUpperCase))
+        );
+
+        /*
+        Map and flatmap practice with string of lines
+         */
+
+        List<String> listOfLines = Arrays.asList( "this is line 1", "this is line 2", "this is line 3");
+        List<List<String>> listOfListOfLines = Arrays.asList( Arrays.asList("this is line 1"), Arrays.asList("this is line 2"), Arrays.asList("this is line 3"));
+
+        System.out.println("Map output");
+        //map is converting individual elements to upper case
+        listOfLines.stream().map(String:: toUpperCase).forEach(System.out::println);
+
+
+        System.out.println("FlatMap output");
+        //flatmap is converting list of list of string to single list of string
+        List<String> flatMapOutput = listOfListOfLines.stream().flatMap(Collection::stream).collect(Collectors.toList());
+        flatMapOutput.forEach(System.out::println);
+
     }
 
 
 }
+
