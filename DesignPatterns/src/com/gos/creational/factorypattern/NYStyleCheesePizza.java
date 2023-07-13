38082c9e60c7fde3961441d5e6dbc88148d1978d
Waseem Hawaldar <waseemhawaldar@gmail.com>
2023-07-13 17:37:18 +0530

    updated

--- DesignPatterns/src/com/gos/creational/factorypattern/NYStyleCheesePizza.java
+++ DesignPatterns/src/com/gos/creational/factorypattern/NYStyleCheesePizza.java
@@ -2,7 +2,6 @@ package com.gos.creational.factorypattern;
 
 public class NYStyleCheesePizza extends Pizza {
 
-	@SuppressWarnings("unchecked")
 	public NYStyleCheesePizza(){
 		name = "NY style sauce and cheese pizza";
 		dough = "Thin Crust Dough";
