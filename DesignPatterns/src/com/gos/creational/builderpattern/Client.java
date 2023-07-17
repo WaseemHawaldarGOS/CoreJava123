aa98b5111a6aad159240c2a63156c4d61f161e36
Waseem Hawaldar <waseemhawaldar@gmail.com>
2023-07-17 10:44:29 +0530

    updated

--- DesignPatterns/src/com/gos/creational/builderpattern/Client.java
+++ DesignPatterns/src/com/gos/creational/builderpattern/Client.java
@@ -7,13 +7,13 @@ public class Client {
 
     public static void main(String args[]){
         DocCreationEngine engine=new DocCreationEngine();
+
         //Creating PDF Document
         PDFDocBuilder pdfDocBuilder=new PDFDocBuilder();
         engine.generateDocument(pdfDocBuilder);
-        PDFDocument pdfDocument=(PDFDocument)pdfDocBuilder.getDocument();
+
         //Creating XML Document
         XMLDocBuilder xmlDocBuilder=new XMLDocBuilder();
         engine.generateDocument(xmlDocBuilder);
-        XMLDocument xmlDocument=(XMLDocument)xmlDocBuilder.getDocument();
     }
 }
