6b77b137441509fdeba1343d2c2ab65c48eab2ae
Waseem Hawaldar <waseemhawaldar@gmail.com>
2023-09-04 15:47:09 -0400

    updated

--- Java8Lambdas/src/com/gos/primitivesverseswrapper/PassByValue.java
+++ Java8Lambdas/src/com/gos/primitivesverseswrapper/PassByValue.java
@@ -2,18 +2,18 @@ package com.gos.primitivesverseswrapper;
 
 public class PassByValue {
 
-    static int data=50;
+    int data=50;
 
     void change(int data){
-        data=data+100;//changes will be in the local variable only
+        data=data+100;//changes will be in the local variable only because primitives like integers are always pass by value
     }
 
     public static void main(String args[]){
         PassByValue op=new PassByValue();
 
-        System.out.println("before change "+data);
-        op.change(data);
-        System.out.println("after change "+data);
+        System.out.println("before change "+op.data);
+        op.change(op.data);
+        System.out.println("after change "+op.data);
 
     }
 }
